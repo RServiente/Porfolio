9b2a6e93a13192d940095c58c15e19b944ab084d
omelo12333 <servienter@gmail.com>
2024-07-27 16:11:48 +0800

    message

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,32 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+}
+
+let slideIndex = 1;
+
+function plusSlides(n, modalId) {
+  showSlides(slideIndex += n, modalId);
+}
+
+function showSlides(n, modalId) {
+  let i;
+  const modal = document.getElementById(modalId);
+  const slides = modal.getElementsByClassName("carousel-slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
